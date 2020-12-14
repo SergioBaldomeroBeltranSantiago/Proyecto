29b49edfd560b5f17fea6ab2eb619fe18c42f833
sergiobaldomerobeltran@gmail.com <sergiobaldomerobeltran@gmail.com>
2020-12-13 19:48:46 -0700

    Programa casi listo, hay que afinar un detalle al momento de copiar archivos y eliminarlos

--- ImagenesPiezas/VentanaPrincipal.Designer.cs
+++ ImagenesPiezas/VentanaPrincipal.Designer.cs
@@ -30,26 +30,102 @@ namespace ImagenesPiezas
         private void InitializeComponent()
         {
             this.ControlBox = new System.Windows.Forms.GroupBox();
+            this.Btn_OpenFolder = new System.Windows.Forms.Button();
+            this.Btn_Rotate = new System.Windows.Forms.Button();
+            this.FolderOpener = new System.Windows.Forms.FolderBrowserDialog();
+            this.Visualizer = new System.Windows.Forms.PictureBox();
+            this.Btn_Imagen_Aprobada = new System.Windows.Forms.Button();
+            this.Btn_Imagen_No_Aprobada = new System.Windows.Forms.Button();
+            this.Btn_Voltear_Imagen = new System.Windows.Forms.Button();
+            this.ControlBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Visualizer)).BeginInit();
             this.SuspendLayout();
             // 
             // ControlBox
             // 
+            this.ControlBox.Controls.Add(this.Btn_OpenFolder);
+            this.ControlBox.Controls.Add(this.Btn_Voltear_Imagen);
+            this.ControlBox.Controls.Add(this.Btn_Rotate);
+            this.ControlBox.Controls.Add(this.Btn_Imagen_No_Aprobada);
+            this.ControlBox.Controls.Add(this.Btn_Imagen_Aprobada);
             this.ControlBox.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ControlBox.Location = new System.Drawing.Point(13, 13);
             this.ControlBox.Name = "ControlBox";
-            this.ControlBox.Size = new System.Drawing.Size(213, 474);
+            this.ControlBox.Size = new System.Drawing.Size(238, 474);
             this.ControlBox.TabIndex = 0;
             this.ControlBox.TabStop = false;
             this.ControlBox.Text = "Controles";
+            // 
+            // Btn_OpenFolder
+            // 
+            this.Btn_OpenFolder.Location = new System.Drawing.Point(6, 45);
+            this.Btn_OpenFolder.Name = "Btn_OpenFolder";
+            this.Btn_OpenFolder.Size = new System.Drawing.Size(217, 37);
+            this.Btn_OpenFolder.TabIndex = 1;
+            this.Btn_OpenFolder.Text = "Seleccionar carpeta";
+            this.Btn_OpenFolder.UseVisualStyleBackColor = true;
+            this.Btn_OpenFolder.Click += new System.EventHandler(this.Btn_OpenFolder_Click);
+            // 
+            // Btn_Rotate
+            // 
+            this.Btn_Rotate.Location = new System.Drawing.Point(6, 114);
+            this.Btn_Rotate.Name = "Btn_Rotate";
+            this.Btn_Rotate.Size = new System.Drawing.Size(217, 42);
+            this.Btn_Rotate.TabIndex = 3;
+            this.Btn_Rotate.Text = "Rotar";
+            this.Btn_Rotate.UseVisualStyleBackColor = true;
+            this.Btn_Rotate.Click += new System.EventHandler(this.Btn_RotateRight_Click);
+            // 
+            // Visualizer
+            // 
+            this.Visualizer.Location = new System.Drawing.Point(310, 13);
+            this.Visualizer.Name = "Visualizer";
+            this.Visualizer.Size = new System.Drawing.Size(658, 474);
+            this.Visualizer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Visualizer.TabIndex = 1;
+            this.Visualizer.TabStop = false;
+            // 
+            // Btn_Imagen_Aprobada
+            // 
+            this.Btn_Imagen_Aprobada.Location = new System.Drawing.Point(6, 228);
+            this.Btn_Imagen_Aprobada.Name = "Btn_Imagen_Aprobada";
+            this.Btn_Imagen_Aprobada.Size = new System.Drawing.Size(217, 38);
+            this.Btn_Imagen_Aprobada.TabIndex = 4;
+            this.Btn_Imagen_Aprobada.Text = "Aprobada";
+            this.Btn_Imagen_Aprobada.UseVisualStyleBackColor = true;
+            this.Btn_Imagen_Aprobada.Click += new System.EventHandler(this.Btn_Imagen_Aprobada_Click);
+            // 
+            // Btn_Imagen_No_Aprobada
+            // 
+            this.Btn_Imagen_No_Aprobada.Location = new System.Drawing.Point(6, 272);
+            this.Btn_Imagen_No_Aprobada.Name = "Btn_Imagen_No_Aprobada";
+            this.Btn_Imagen_No_Aprobada.Size = new System.Drawing.Size(217, 36);
+            this.Btn_Imagen_No_Aprobada.TabIndex = 5;
+            this.Btn_Imagen_No_Aprobada.Text = "No Aprobada";
+            this.Btn_Imagen_No_Aprobada.UseVisualStyleBackColor = true;
+            this.Btn_Imagen_No_Aprobada.Click += new System.EventHandler(this.Btn_Imagen_No_Aprobada_Click);
+            // 
+            // Btn_Voltear_Imagen
+            // 
+            this.Btn_Voltear_Imagen.Location = new System.Drawing.Point(6, 162);
+            this.Btn_Voltear_Imagen.Name = "Btn_Voltear_Imagen";
+            this.Btn_Voltear_Imagen.Size = new System.Drawing.Size(217, 42);
+            this.Btn_Voltear_Imagen.TabIndex = 6;
+            this.Btn_Voltear_Imagen.Text = "Voltear";
+            this.Btn_Voltear_Imagen.UseVisualStyleBackColor = true;
+            this.Btn_Voltear_Imagen.Click += new System.EventHandler(this.Btn_Voltear_Imagen_Click);
             // 
             // VentanaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1154, 499);
+            this.ClientSize = new System.Drawing.Size(978, 499);
+            this.Controls.Add(this.Visualizer);
             this.Controls.Add(this.ControlBox);
             this.Name = "VentanaPrincipal";
             this.Text = "Imagen";
+            this.ControlBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Visualizer)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +133,13 @@ namespace ImagenesPiezas
         #endregion
 
         private System.Windows.Forms.GroupBox ControlBox;
+        private System.Windows.Forms.Button Btn_OpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog FolderOpener;
+        private System.Windows.Forms.PictureBox Visualizer;
+        private System.Windows.Forms.Button Btn_Rotate;
+        private System.Windows.Forms.Button Btn_Imagen_Aprobada;
+        private System.Windows.Forms.Button Btn_Imagen_No_Aprobada;
+        private System.Windows.Forms.Button Btn_Voltear_Imagen;
     }
 }
 
--- ImagenesPiezas/VentanaPrincipal.cs
+++ ImagenesPiezas/VentanaPrincipal.cs
@@ -7,14 +7,112 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Collections;
 
 namespace ImagenesPiezas
 {
     public partial class VentanaPrincipal : Form
     {
+
+        string folderpath;
+        string[] filepaths;
+        ArrayList filepathss = new ArrayList();
+        Image ori_imagen, mod_imagen;
         public VentanaPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void Btn_OpenFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = FolderOpener.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderpath = FolderOpener.SelectedPath;
+                filepaths = Directory.GetFiles(folderpath);
+                if (filepaths.Length > 0)
+                {
+                    for (int i = 0; i < filepaths.Length; i++)
+                    {
+                        filepathss.Add(filepaths[i]);
+                    }
+                    Btn_OpenFolder.Enabled = false;
+                }
+                CreateImage((string)filepathss[0]);
+            }
+        }
+
+        private void Btn_RotateRight_Click(object sender, EventArgs e)
+        {
+            RotateImageRight();
+        }
+
+        private void Btn_Voltear_Imagen_Click(object sender, EventArgs e)
+        {
+            RotateImageHalfCircle();
+        }
+
+        private void Btn_Imagen_Aprobada_Click(object sender, EventArgs e)
+        {
+            DialogResult resulttwo = MessageBox.Show("Esta a punto de aprobar una imagen\n¿Esta seguro de esto? ", "Confirmacion", MessageBoxButtons.YesNo);
+            if (resulttwo == DialogResult.Yes)
+            {
+                if (!File.Exists(folderpath + "\\Imagenes Aprobadas"))
+                {
+                    Directory.CreateDirectory(folderpath + "\\Imagenes Aprobadas");
+                }
+                Visualizer.Image = null;
+                string ori_directory = (string)filepathss[0];
+                string new_directory = folderpath + "\\Imagenes Aprobadas\\" + Path.GetFileName((string)filepathss[0]);
+                File.Copy(ori_directory, new_directory);
+                ImageRotator();
+            }
+        }
+
+        private void Btn_Imagen_No_Aprobada_Click(object sender, EventArgs e)
+        {
+            DialogResult resultthree = MessageBox.Show("Esta a punto de desaprobar una imagen\n¿Esta seguro de esto? ", "Confirmacion", MessageBoxButtons.YesNo);
+            if (resultthree == DialogResult.Yes)
+            {
+                if (!File.Exists(folderpath + "\\Imagenes No Aprobadas"))
+                {
+                    Directory.CreateDirectory(folderpath + "\\Imagenes No Aprobadas");
+                }
+                Visualizer.Image = null;
+                string ori_directory = (string)filepathss[0];
+                string new_directory = folderpath + "\\Imagenes No Aprobadas\\" + Path.GetFileName((string)filepathss[0]);
+                File.Copy(ori_directory, new_directory);
+                ImageRotator();
+            }
+        }
+
+
+        public void RotateImageHalfCircle()
+        {
+            mod_imagen.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            Visualizer.Image = mod_imagen;
+        }
+
+
+        public void ImageRotator()
+        {
+            filepathss.RemoveAt(0);
+            CreateImage((string)filepathss[0]);
+        }
+        public void CreateImage(string path)
+        {
+            ori_imagen = Image.FromFile(path);
+            Visualizer.Image = ori_imagen;
+            mod_imagen = ori_imagen;
+        }
+
+        public void RotateImageRight()
+        {
+            mod_imagen.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            Visualizer.Image = mod_imagen;
         }
     }
 }
